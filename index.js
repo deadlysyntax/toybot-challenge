7e699b5d394fbbca05a5dab0dafc3820a4ded979
Jaap <jaap@deadlysyntax.com>
2017-11-13 11:14:57 +1100

    add runCommand function

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const inquirer     = require('inquirer')
 const command      = require('./libs/command').command
 const environment  = require('./libs/board') 
 
+
+
 let init = async () => {
     let board         = environment.board
     try {
         let commandObject = await nextCommand(board)
-        console.log(commandObject)
+        processCommand(commandObject, board)
     }
     catch(e){
         console.log(e)
     }
-    
-   
 }
+
+
 
 
 let nextCommand = async (board) => {
@@ -23,12 +25,26 @@ let nextCommand = async (board) => {
 
 
 
+
 // Make sure the user input is a valid command
 let processCommand = (commandObject, board) => {
     if( ! command.valid(commandObject) ) {
         console.log('Invalid command, please try again.')
         nextCommand(board)
     }
+    else {
+        runCommand(commandObject, board)
+    }
+}
+
+
+
+let runCommand = async (commandObject, board) => {
+    // Calls the command method (based on input command from user)
+    // which returns a response object with either and error or 
+    // a new board object
+    let newBoardState = command[commandObject.command](commandObject, board)
+    console.log(newBoardState)
 }
 
 
