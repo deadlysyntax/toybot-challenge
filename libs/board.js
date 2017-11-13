bfb6dc5142ecb9e0e81841574fd6e03cdddf6c8d
Jaap <jaap@deadlysyntax.com>
2017-11-13 11:42:19 +1100

    added functions for validating position, direction and values for calculating both

--- libs/board.js
+++ libs/board.js
@@ -5,5 +5,22 @@ module.exports = {
         x: null,
         y: null,
         d: null
-    }
+    },
+    position: {
+        directionMatrix: {
+            n: { axis: 'y', traj: 1,  turns: { left: 'w', right: 'e'} },
+            s: { axis: 'y', traj: -1, turns: { left: 'e', right: 'w'} },
+            e: { axis: 'x', traj: 1,  turns: { left: 'n', right: 's'} },
+            w: { axis: 'x', traj: -1, turns: { left: 's', right: 'n'} }
+        }
+    },
+    validPosition: (position) => {
+        return position.x > -1
+            && position.x < this.board.size
+            && position.y > -1
+            && position.y < this.board.size
+    },
+    validDirection: (position) => {
+        return ['n','s','e','w'].indexOf(position.d.toLowerCase()) > -1
+    },
 }
